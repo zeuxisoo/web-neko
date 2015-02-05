0766299269fb767abb9f00894dbd4be552dbbbd2
Zeuxis Lo <seekstudio@gmail.com>
2015-02-06 02:17:51 +0800

    fix nl2br filter can not working

--- neko/app.py
+++ neko/app.py
@@ -93,9 +93,8 @@ def register_template_filter(app):
     @app.template_filter()
     @evalcontextfilter
     def nl2br(eval_ctx, value):
-        _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-        result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') for p in _paragraph_re.split(escape(value)))
+        _paragraph_re = re.compile(r'(?:\r\n|\r(?!\n)|\n){2,}')
+        result = u'\n\n'.join(u'<p>%s</p>' % p.replace(u'\n', Markup('<br>\n')) for p in _paragraph_re.split(value))
         if eval_ctx.autoescape:
             result = Markup(result)
         return result
