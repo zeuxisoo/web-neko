94394c823f31c01d9a4c337f41129d8d3f6779bb
Zeuxis Lo <seekstudio@gmail.com>
2015-03-21 21:36:45 +0800

    remove bootstrap, add materializecss and try to re-design index page

--- static/client/js/default.js
+++ static/client/js/default.js
@@ -1,33 +1,39 @@
-(function($) {
+// (function($) {
 
-	FormStorage.init();
+// 	FormStorage.init();
 
-	$(function() {
-		// Textarea auto expand
-		$('.autosize').autosize({
-			append: "\n"
-		});
+// 	$(function() {
+// 		// Textarea auto expand
+// 		$('.autosize').autosize({
+// 			append: "\n"
+// 		});
 
-		// Restore form data
-		FormStorage.restore();
+// 		// Restore form data
+// 		FormStorage.restore();
 
-		// Placeholder style
-		$("[placeholder]").focusout(function() {
-			if ($(this).val() == '' || $(this).val() == $(this).prop("placeholder")) {
-				$(this).addClass("bold-input")
-			}
-		}).bind('change paste keyup', function() {
-			if ($(this).val() != '') {
-				$(this).removeClass("bold-input")
-			}else{
-				$(this).addClass("bold-input")
-			}
-		}).focusout();
+// 		// Placeholder style
+// 		$("[placeholder]").focusout(function() {
+// 			if ($(this).val() == '' || $(this).val() == $(this).prop("placeholder")) {
+// 				$(this).addClass("bold-input")
+// 			}
+// 		}).bind('change paste keyup', function() {
+// 			if ($(this).val() != '') {
+// 				$(this).removeClass("bold-input")
+// 			}else{
+// 				$(this).addClass("bold-input")
+// 			}
+// 		}).focusout();
 
-		// Delete confirm
-		$("[data-confirm]").on('click', function(e) {
-			return window.confirm("Are you sure delete this data?")
-		});
+// 		// Delete confirm
+// 		$("[data-confirm]").on('click', function(e) {
+// 			return window.confirm("Are you sure delete this data?")
+// 		});
+// 	});
+
+// })(jQuery);
+
+(function($){
+	$(function(){
+		$('.button-collapse').sideNav();
 	});
-
 })(jQuery);
