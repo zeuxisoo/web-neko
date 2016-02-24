187be69426bb284a35b5bbb4bc9228962d84a0f7
Zeuxis Lo <seekstudio@gmail.com>
2016-02-24 17:50:07 +0800

    show activity label list in activity main page

--- resources/assets/views/activity/index.vue
+++ resources/assets/views/activity/index.vue
@@ -1,16 +1,64 @@
 <template>
     <div id="activity-index">
         <div class="panel panel-default">
-            <div class="panel-heading">Activity Index</div>
+            <div class="panel-heading">Activity Labels</div>
             <div class="panel-body">
-                Content
+                <div class="row">
+                    <div class="col-sm-12">
+                        <span class="btn btn-xs btn-default label-space" v-for="label in labels">
+                            <a >{{ label.name }}</a>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style>
+.label-space {
+    margin: 0px 2px;
+}
+
+.label-space a {
+    text-decoration: none;
+}
 </style>
 
 <script>
+export default {
+
+    data() {
+        return {
+            labels: []
+        }
+    },
+
+    route: {
+
+        data() {
+            this.fetchLabels();
+        }
+
+    },
+
+    methods: {
+
+        fetchLabels() {
+            this.$api.activity.allLabel().then(
+                (response) => {
+                    let data    = response.data;
+                    let labels  = data.data;
+
+                    this.labels = labels;
+                },
+                (response) => {
+                    MessageHelper.error('Cannot fetch activity label list');
+                }
+            );
+        },
+
+    }
+
+}
 </script>
