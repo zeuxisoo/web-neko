80ab68fbeb8c459db37f2009ce085df6ec5333e6
Zeuxis Lo <seekstudio@gmail.com>
2016-03-04 14:39:22 +0800

    improve activity view in mobile layout

--- resources/assets/views/activity.vue
+++ resources/assets/views/activity.vue
@@ -1,7 +1,29 @@
 <template>
     <div id="activity">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-xs-12 col-sm-12 visible-xs-block visible-sm-block">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Actions</div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-3 col-sm-2 text-right">Activity:</div>
+                            <div class="col-xs-9 col-sm-10 col-actions">
+                                <a v-link="{ name: 'activity_index' }" class="btn btn-xs btn-default">All</a>
+                                <a v-link="{ name: 'activity_create' }" class="btn btn-xs btn-default">Create</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-3 col-sm-2 text-right">Label:</div>
+                            <div class="col-xs-9 col-sm-10 col-actions">
+                                <a v-link="{ name: 'activity_label_create' }" class="btn btn-xs btn-default">All</a>
+                                <a v-link="{ name: 'activity_label_manage' }" class="btn btn-xs btn-default">Manage</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 hidden-xs hidden-sm">
                 <div class="panel panel-default">
                     <div class="panel-heading">Activity</div>
                     <div class="panel-body">
@@ -21,7 +43,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-9">
+
+            <div class="col-xs-12 col-sm-12 col-md-9">
                 <router-view></router-view>
             </div>
         </div>
@@ -29,6 +52,9 @@
 </template>
 
 <style>
+.col-actions {
+    padding-left: 0px;
+}
 </style>
 
 <script>
