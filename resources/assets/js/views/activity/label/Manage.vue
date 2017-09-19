de9768d4bda9e13b86ee5de4e2fbb3110a4309f1
Zeuxis Lo <seekstudio@gmail.com>
2017-09-19 16:10:52 +0800

    add back activity label manage function

--- resources/assets/js/views/activity/label/Manage.vue
+++ resources/assets/js/views/activity/label/Manage.vue
@@ -1,6 +1,32 @@
 <template>
     <div id="activity-label-manage">
-
+        <div id="activity-label-create">
+            <div class="panel panel-default">
+                <div class="panel-heading">Manage Label</div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Manage</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="label in labels">
+                                    <td>{{ label.id }}</td>
+                                    <td>{{ label.name }}</td>
+                                    <td>
+                                        <a v-on:click="destory(label.id)">Delete</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,7 +34,54 @@
 </style>
 
 <script>
+import MessageHelper from '../../../helpers/message'
+
 export default {
+    name: 'activity-label-manage',
+
+    data() {
+        return {
+            labels: []
+        }
+    },
+
+    created() {
+        this.fetchLabels()
+    },
+
+    watch: {
+        $route() {
+            this.fetchLabels()
+        }
+    },
+
+    methods: {
+        fetchLabels() {
+            this.$api.activity
+            .allLabel()
+            .then(response => {
+                let data    = response.data
+                let labels  = data.data
+
+                this.labels = labels
+            }).catch(error => {
+                MessageHelper.error('Cannot fetch activity label list')
+            })
+        },
+
+        destory(id) {
+            this.$api.activity
+            .deleteLabel({
+                id: id
+            }).then(response => {
+                this.fetchLabels()
+
+                MessageHelper.success('The label deleted')
+            }).catch(error => {
+                MessageHelper.error('Cannot delete label')
+            })
+        }
+    }
 
 }
 </script>
