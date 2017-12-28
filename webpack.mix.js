83f9a919f50f9f6c62a065b75bce667fcdb3cc3f
Zeuxis Lo <seekstudio@gmail.com>
2017-12-28 23:09:30 +0800

    enable versioning when generate assets in production mode

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-// if (mix.inProduction()) {
-//     mix.version();
-// }
+if (mix.inProduction()) {
+    mix.version();
+}
