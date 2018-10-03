a88560d3526295b814699baafcbb7041fac91ea6
Zeuxis Lo <seekstudio@gmail.com>
2018-10-03 14:04:34 +0800

    fix the indentation

--- switch.sh
+++ switch.sh
@@ -4,58 +4,58 @@ RESOURCES_VIEWS_PATH=resources/views
 OPEN_STATUS_FILE=$RESOURCES_VIEWS_PATH/.openning
 
 function open_home {
-	if [ ! -f $OPEN_STATUS_FILE ]; then
-		# Append .close suffix to close page
-		mv $RESOURCES_VIEWS_PATH/index.blade.php $RESOURCES_VIEWS_PATH/index.blade.php.close
+    if [ ! -f $OPEN_STATUS_FILE ]; then
+        # Append .close suffix to close page
+        mv $RESOURCES_VIEWS_PATH/index.blade.php $RESOURCES_VIEWS_PATH/index.blade.php.close
 
-		# Remove .open suffix from open page
-		mv $RESOURCES_VIEWS_PATH/index.blade.php.open $RESOURCES_VIEWS_PATH/index.blade.php
+        # Remove .open suffix from open page
+        mv $RESOURCES_VIEWS_PATH/index.blade.php.open $RESOURCES_VIEWS_PATH/index.blade.php
 
-		clean_views_cache
+        clean_views_cache
 
-		touch $OPEN_STATUS_FILE
+        touch $OPEN_STATUS_FILE
 
-		echo "OK"
-	else
-		echo "Error! The page is not close"
-	fi
+        echo "OK"
+    else
+        echo "Error! The page is not close"
+    fi
 }
 
 function close_home {
-	if [ -f $OPEN_STATUS_FILE ]; then
-		# Append .open suffix to open page
-		mv $RESOURCES_VIEWS_PATH/index.blade.php $RESOURCES_VIEWS_PATH/index.blade.php.open
+    if [ -f $OPEN_STATUS_FILE ]; then
+        # Append .open suffix to open page
+        mv $RESOURCES_VIEWS_PATH/index.blade.php $RESOURCES_VIEWS_PATH/index.blade.php.open
 
-		# Remove .close suffix from close page
-		mv $RESOURCES_VIEWS_PATH/index.blade.php.close $RESOURCES_VIEWS_PATH/index.blade.php
+        # Remove .close suffix from close page
+        mv $RESOURCES_VIEWS_PATH/index.blade.php.close $RESOURCES_VIEWS_PATH/index.blade.php
 
-		clean_views_cache
+        clean_views_cache
 
-		rm -rf $OPEN_STATUS_FILE
+        rm -rf $OPEN_STATUS_FILE
 
         echo "OK"
-	else
-		echo "Error! The page is not open"
-	fi
+    else
+        echo "Error! The page is not open"
+    fi
 }
 
 function clean_views_cache {
-	rm -rf storage/framework/views/*.php
+    rm -rf storage/framework/views/*.php
 }
 
 COMMAND=${@:$OPTIND:1}
 
 case $COMMAND in
-	on)
-		echo "On ..."
-		open_home
-	;;
-	off)
-		echo "Off ..."
-		close_home
-	;;
-	*)
-		echo "- on"
-		echo "- off"
-	;;
+    on)
+        echo "On ..."
+        open_home
+    ;;
+    off)
+        echo "Off ..."
+        close_home
+    ;;
+    *)
+        echo "- on"
+        echo "- off"
+    ;;
 esac
