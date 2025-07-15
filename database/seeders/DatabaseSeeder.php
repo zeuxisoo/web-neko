ddbbefa1d396e00215841e062070e485bf298b11
Zeuxis Lo <seekstudio@gmail.com>
2025-07-15 20:42:07 +0800

    all: format the code style

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,11 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         // User::factory(10)->create();
 
         User::factory()->create([
