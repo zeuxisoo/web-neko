57bc9df51481801e97cd886913851f6bf16a4cb3
Zeuxis Lo <seekstudio@gmail.com>
2025-10-10 22:08:08 +0800

    dashboard: change the theme select of dropbox menu to theme switcher component

--- resources/js/components/dashboard/Home.vue
+++ resources/js/components/dashboard/Home.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import { useColorMode } from '@vueuse/core';
-import { Button } from '@/components/base/button';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from '@/components/base/dropdown-menu';
 import { Separator } from '@/components/base/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/base/sidebar';
 import AppBar from '@/components/dashboard/home/AppBar.vue';
-
-const mode = useColorMode();
+import ThemeSwitcher from '@/components/dashboard/home/ThemeSwitcher.vue';
 </script>
 
 <template>
@@ -21,20 +12,12 @@ const mode = useColorMode();
             <header
                 class="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
             >
-                <div class="flex w-full items-center gap-2 px-3 h-4">
+                <div class="flex h-4 w-full items-center gap-2 px-3">
                     <SidebarTrigger class="-ml-1" />
                     <Separator orientation="vertical" class="mr-2 h-4" />
                     <h1 className="text-base font-medium">Yowl</h1>
                     <div className="ml-auto flex items-center gap-2">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger as-child>
-                                <Button variant="ghost" size="sm"> Theme </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                                <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
-                                <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                        <ThemeSwitcher />
                     </div>
                 </div>
             </header>
---
+++ resources/js/components/dashboard/home/ThemeSwitcher.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Button } from '@/components/base/button';
+import { useColorMode } from '@vueuse/core';
+import { Moon, Sun } from 'lucide-vue-next';
+import { computed } from 'vue';
+
+const mode = useColorMode();
+
+const isDark = computed(() => mode.value === 'dark');
+
+const toggleDark = () => {
+    mode.value = mode.value === 'light' ? 'dark' : 'light';
+};
+</script>
+
+<template>
+    <Button variant="ghost" size="icon" class="hidden sm:flex" @click="toggleDark">
+        <component :is="isDark ? Sun : Moon" />
+        <span class="sr-only">Toggle theme</span>
+    </Button>
+</template>
