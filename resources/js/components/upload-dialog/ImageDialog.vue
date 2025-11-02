e523678f894f47a427fa6ee7b8b651f45e72473e
Zeuxis Lo <seekstudio@gmail.com>
2025-11-02 17:10:35 +0800

    component: add more config in image upload dialog (e.g. server url, size/type check and label text)

--- resources/js/components/upload-dialog/ImageDialog.vue
+++ resources/js/components/upload-dialog/ImageDialog.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { ImageUp } from 'lucide-vue-next';
+import { CircleX, ImageUp, Upload } from 'lucide-vue-next';
 import { Button } from '@/components/base/button';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/base/dialog';
 import VueFilePond from 'vue-filepond';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
 const FilePond = VueFilePond(
+    FilePondPluginFileValidateSize,
     FilePondPluginFileValidateType,
     FilePondPluginImagePreview,
 );
@@ -26,12 +28,26 @@ const FilePond = VueFilePond(
             </DialogHeader>
             <div class="flex items-center space-x-2">
                 <div class="grid flex-1 gap-2">
-                    <FilePond allow-multiple="true" image-preview-max-height="80" />
+                    <FilePond
+                        allow-multiple="true"
+                        server="/pulse/upload-images"
+                        instant-upload="false"
+                        max-file-size="8MB"
+                        label-max-file-size="Only allow {filesize}"
+                        accepted-file-types="image/jpeg, image/jpg, image/png, image/webp"
+                        label-file-type-not-allowed="Wrong file type"
+                        file-validate-type-label-expected-types="Expects images"
+                        image-preview-max-height="80" />
                 </div>
             </div>
-            <DialogFooter class="sm:justify-start">
+            <DialogFooter class="grid grid-cols-2">
+                <Button type="button">
+                    <Upload /> Upload
+                </Button>
                 <DialogClose as-child>
-                    <Button type="button">OK</Button>
+                    <Button type="button" variant="secondary">
+                        <CircleX /> Close
+                    </Button>
                 </DialogClose>
             </DialogFooter>
         </DialogContent>
