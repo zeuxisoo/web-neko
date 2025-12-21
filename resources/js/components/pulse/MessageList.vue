5f737f9ea556ae5f6122067465db30f57c70eab6
Zeuxis Lo <seekstudio@gmail.com>
2025-12-21 14:53:31 +0800

    pulse: add skeleton as placeholder for the remaining image

--- resources/js/components/pulse/MessageList.vue
+++ resources/js/components/pulse/MessageList.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import { Card, CardHeader } from '@/components/base/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/base/avatar';
-import { Bookmark, Ellipsis, MessageSquareMore } from 'lucide-vue-next';
 import { Badge } from '@/components/base/badge';
+import { Card, CardHeader } from '@/components/base/card';
+import { Skeleton } from '@/components/base/skeleton';
+import { Bookmark, Ellipsis, MessageSquareMore } from 'lucide-vue-next';
 
 const remoteImages = [
     { src: 'https://placecats.com/512/512', title: 'cat1' },
     { src: 'https://placecats.com/512/512', title: 'cat1' },
     { src: 'https://placecats.com/512/512', title: 'cat1' },
     { src: 'https://placecats.com/512/512', title: 'cat1' },
-]
+];
+
+const totalRemoteImages = Math.round(6 / remoteImages.length);
+const placeHolderImages = new Array(totalRemoteImages).fill(0);
 </script>
 
 <template>
@@ -27,31 +31,39 @@ const remoteImages = [
             </div>
             <div class="grid gap-2">
                 <div class="font-light">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </div>
-                <div class="columns-2 md:columns-6">
+                <div class="grid grid-cols-2 gap-2 md:grid-cols-6">
                     <div class="flex justify-center" v-for="(image, i) in remoteImages" :key="i">
-                        <img :src="image.src" :title="image.title" class="object-cover max-h-48 cursor-pointer border border-border/60 rounded-lg" decoding="async" loading="lazy" />
+                        <img
+                            :src="image.src"
+                            :title="image.title"
+                            class="max-h-48 cursor-pointer rounded-lg border object-cover"
+                            decoding="async"
+                            loading="lazy"
+                        />
                     </div>
+                    <template v-for="i in placeHolderImages" :key="i">
+                        <div class="flex justify-center">
+                            <Skeleton class="aspect-square max-h-48 w-48 cursor-pointer rounded-lg border" />
+                        </div>
+                    </template>
                 </div>
                 <div class="flex flex-row gap-2">
-                    <Badge variant="secondary" v-for="i in [1, 2, 3, 4]" :key="i">
-                        #Tag1
-                    </Badge>
+                    <Badge variant="secondary" v-for="i in [1, 2, 3, 4]" :key="i"> #Tag1 </Badge>
                 </div>
                 <div class="grid grid-cols-3 gap-2">
                     <div class="comment">
-                        <div class="inline-flex items-center justify-center gap-2 text-sm font-medium py-2">
-                            <MessageSquareMore :size="16" /> 0
-                        </div>
+                        <div class="inline-flex items-center justify-center gap-2 py-2 text-sm font-medium"><MessageSquareMore :size="16" /> 0</div>
                     </div>
                     <div class="bookmark flex justify-center">
-                        <div class="inline-flex items-center justify-center gap-2 text-sm font-medium py-2">
+                        <div class="inline-flex items-center justify-center gap-2 py-2 text-sm font-medium">
                             <Bookmark :size="16" />
                         </div>
                     </div>
                     <div class="action flex justify-end">
-                        <div class="inline-flex items-center justify-center gap-2 text-sm font-medium py-2">
+                        <div class="inline-flex items-center justify-center gap-2 py-2 text-sm font-medium">
                             <Ellipsis :size="16" />
                         </div>
                     </div>
