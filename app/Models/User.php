948bc3bbc29c7565997052f05ed4bb60f473fc44
Zeuxis Lo <seekstudio@gmail.com>
2025-12-22 14:48:49 +0800

    backend: rename name to username in users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable {
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
