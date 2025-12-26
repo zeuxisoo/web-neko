73165b2891fd9b4fa3930538ab3205ebc8dad3a1
Zeuxis Lo <seekstudio@gmail.com>
2025-12-26 22:16:48 +0800

    helper: remove default fallback message

--- resources/js/helpers/whoops.ts
+++ resources/js/helpers/whoops.ts
@@ -1,12 +1,16 @@
-import { ValidateError } from "@/validators";
-import { toast } from "vue-sonner";
+import { ValidateError } from '@/validators';
+import { toast } from 'vue-sonner';
 
 export default class WhoopsHandler {
 
-    static handleError(error: unknown | ValidateError, fallbackMessage = "Unknown error on whoops handler") {
+    static handleError(error: unknown | ValidateError, fallbackMessage: string) {
         if (error instanceof ValidateError) {
             toast.error(error.message);
             return;
+        }
+
+        if (!fallbackMessage || fallbackMessage === '') {
+            fallbackMessage = 'Unknown error on whoops handler';
         }
 
         toast.error(fallbackMessage);
