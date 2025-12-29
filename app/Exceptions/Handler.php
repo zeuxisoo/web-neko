f3d7a0c78dc39b882f8a477264befc54a772a7ce
Zeuxis Lo <seekstudio@gmail.com>
2025-12-29 21:15:12 +0800

    backend: add ok field into unauthenticated response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -14,5 +16,16 @@ class Handler extends ExceptionHandler
             'message' => $exception->getMessage(),
             'errors' => $exception->errors(),
         ], $exception->status);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception): Response {
+        if ($this->shouldReturnJson($request, $exception)) {
+            return response()->json([
+                'ok' => false,
+                'message' => $exception->getMessage(),
+            ], 401);
+        }
+
+        return redirect()->guest($exception->redirectTo($request) ?? route('login'));
     }
 }
