d9c2ddeb7db4934e40428dd12a5755a1824e5f0b
Zeuxis Lo <seekstudio@gmail.com>
2026-01-07 20:57:24 +0800

    component: fix avatar is undefined warning in sidebar user area

--- resources/js/components/sidebar/NavUser.vue
+++ resources/js/components/sidebar/NavUser.vue
@@ -56,7 +56,7 @@ const handleLogout = async () => {
                 <DropdownMenuTrigger as-child>
                     <SidebarMenuButton size="lg" class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
                         <Avatar class="h-8 w-8 rounded-lg">
-                            <AvatarImage :src="props.user.avatar" :alt="props.user.username" />
+                            <AvatarImage :src="props.user.avatar" :alt="props.user.username" v-if="props.user.avatar" />
                             <AvatarFallback class="rounded-lg"> AV </AvatarFallback>
                         </Avatar>
                         <div class="grid flex-1 text-left text-sm leading-tight">
@@ -75,7 +75,7 @@ const handleLogout = async () => {
                     <DropdownMenuLabel class="p-0 font-normal">
                         <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                             <Avatar class="h-8 w-8 rounded-lg">
-                                <AvatarImage :src="props.user.avatar" :alt="props.user.username" />
+                                <AvatarImage :src="props.user.avatar" :alt="props.user.username" v-if="props.user.avatar" />
                                 <AvatarFallback class="rounded-lg"> AV </AvatarFallback>
                             </Avatar>
                             <div class="grid flex-1 text-left text-sm leading-tight">
