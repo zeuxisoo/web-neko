330c2e6d8569358364a7ead8d6b3c5e80cb8c80c
Zeuxis Lo <seekstudio@gmail.com>
2026-01-17 19:29:21 +0800

    account: upload the avatar column after avatar uploaded

--- app/Api/Version1/Controllers/Account/ProfileController.php
+++ app/Api/Version1/Controllers/Account/ProfileController.php
@@ -5,7 +5,10 @@ namespace App\Api\Version1\Controllers\Account;
 use App\Api\Version1\Bases\ApiController;
 use App\Api\Version1\Requests\Account\Profile\UpdateRequest;
 use App\Api\Version1\Requests\Account\Profile\UploadAvatarRequest;
+use App\Api\Version1\Resources\Auth\UserResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -27,22 +30,25 @@ class ProfileController extends ApiController
         return $this->respondJsonMessage('Successfully updated profile, Please Login again.');
     }
 
-    public function uploadAvatar(UploadAvatarRequest $request): JsonResponse {
+    public function uploadAvatar(UploadAvatarRequest $request): JsonResource {
         $file = $request->file('file');
 
+        // generate filename and store path
         $filename = strtolower((string) Str::ulid()).'_'.Str::random(8).'.'.$file->getClientOriginalExtension();
         $storePath = 'avatar/'.$filename;
 
+        // create thumb image
         $manager = new ImageManager(new Driver());
         $image = $manager->read($file)->scaleDown(96, 96);
 
+        // save to store path
         Storage::disk('public')->put($storePath, (string) $image->encode());
 
-        // TODO: to resource
-        return $this->respondJsonData([
-            'filename' => $filename,
-            'path' => $storePath,
-            'url' => Storage::disk('public')->url($storePath),
-        ]);
+        // update avatar
+        $user = User::find($this->user()->id);
+        $user->avatar = $filename;
+        $user->save();
+
+        return new UserResource($user);
     }
 }
