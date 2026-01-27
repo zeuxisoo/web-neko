1bb6acc81d65516b8467698cf7025a2e1d1ed253
Zeuxis Lo <seekstudio@gmail.com>
2026-01-27 13:44:22 +0800

    api: fix logout order and token deletion to work between web and sanctum api guard

--- app/Api/Version1/Controllers/Auth/LogoutController.php
+++ app/Api/Version1/Controllers/Auth/LogoutController.php
@@ -3,6 +3,7 @@
 namespace App\Api\Version1\Controllers\Auth;
 
 use App\Api\Version1\Bases\ApiController;
+use App\Models\UserAccessToken;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,10 +11,16 @@ use Illuminate\Support\Facades\Auth;
 class LogoutController extends ApiController
 {
     public function destroy(Request $request): JsonResponse {
-        Auth::guard('web')->logout();
-
         $user = $request->user();
-        $user->currentAccessToken()->delete();
+        $currentToken = $user->currentAccessToken();
+
+        if (method_exists($currentToken, 'delete')) {
+            $currentToken->delete();
+        } else {
+            UserAccessToken::find($currentToken->id)->delete();
+        }
+
+        Auth::guard('web')->logout();
 
         return $this->respondJsonMessage('Successfully logged out');
     }
