3ff652438f65db9683a9028f33b40a5b0bf5c040
Zeuxis Lo <seekstudio@gmail.com>
2026-02-06 17:44:56 +0800

    pulse: display active tag filter in memo list when tag is not empty

--- resources/js/components/pulse/MemoList.vue
+++ resources/js/components/pulse/MemoList.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
 import { Alert, AlertDescription, AlertTitle } from '@/components/base/alert';
+import { Button } from '@/components/base/button';
 import { Card, CardContent } from '@/components/base/card';
-import { Info } from 'lucide-vue-next';
+import { CircleX, Info } from 'lucide-vue-next';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import MemoItem from './MemoItem.vue';
 import MemoPagination from './MemoPagination.vue';
 
 const props = defineProps<{
     memos: PulseMemoIndexResponse | undefined;
 }>();
+
+const route = useRoute();
+
+const hasTag = computed(() => {
+    return route.query.tag && route.query.tag !== '';
+});
 </script>
 
 <template>
+    <div class="grid grid-cols-1">
+        <Card class="p-1.5" v-if="hasTag">
+            <CardContent>
+                <div class="flex flex-wrap items-center gap-2">
+                    <div class="font-bold">Filter:</div>
+                    <Button variant="outline" size="sm" class="round-md">
+                        Tag
+                        <CircleX />
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
     <div class="grid grid-cols-1 gap-2" v-if="props.memos">
         <MemoItem :memo="memo" v-for="memo in props.memos.data" />
         <MemoPagination :links="props.memos.links" :meta="props.memos.meta" />
