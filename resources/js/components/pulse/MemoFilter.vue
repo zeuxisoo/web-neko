4b19494a7826110badd802cca2e10bd0e9f6e380
Zeuxis Lo <seekstudio@gmail.com>
2026-02-06 18:00:16 +0800

    component: extract memo filter to MemoFilter component to handle remove tag and show

---
+++ resources/js/components/pulse/MemoFilter.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Button } from '@/components/base/button';
+import { Card, CardContent } from '@/components/base/card';
+import { CircleX } from 'lucide-vue-next';
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const hasTag = computed(() => {
+    return route.query.tag && route.query.tag !== '';
+});
+
+const handleRemoveTag = () => {
+    // set tag to empty when use push
+    // delete route.query.tag should be use replace
+    router.push({
+        name: 'park.pulse',
+        query: {
+            ...route.query,
+            tag: '',
+        },
+    });
+};
+</script>
+
+<template>
+    <Card class="p-1.5" v-if="hasTag">
+        <CardContent>
+            <div class="flex flex-wrap items-center gap-2">
+                <div class="font-bold">Filter:</div>
+                <Button variant="outline" size="sm" class="round-md" @click="handleRemoveTag">
+                    Tag
+                    <CircleX />
+                </Button>
+            </div>
+        </CardContent>
+    </Card>
+</template>
