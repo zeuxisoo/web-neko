88dbcec4dd8f609010fb76339c378f0b5b8e9086
Zeuxis Lo <seekstudio@gmail.com>
2026-02-16 18:04:47 +0800

    backend: fix `resources/js/app.js` cannot be found

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
 @endif
 </head>
 <body class="bg-background overscroll-none font-sans antialiased">
